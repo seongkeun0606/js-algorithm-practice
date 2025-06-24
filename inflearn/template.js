3f23eb4844f858ae5ac2013ad0a9b4891eadcce5
seongkeun0606 <wkdrnslasla1@naver.com>
2025-06-24 12:18:18 +0900

    chore: 인프런 강의 문제풀이용 프로젝트 세팅

---
+++ inflearn/template.js
@@ -0,0 +1,25 @@
+/**
+ * 📌 문제 제목: 
+ * 
+ * 문제 설명:
+ * 
+ * 
+ * 입력 예시:
+ * 
+ * 출력 예시:
+ * 
+ */
+
+// 🧑 내 풀이
+function mySolution() {
+}
+
+// 👨‍🏫 선생님 풀이
+function teacherSolution() {
+}
+
+// ✅ 테스트 코드
+const input = [];
+
+console.log('🧑 내 답:', mySolution(input));
+console.log('👨‍🏫 선생님 답:', teacherSolution(input));
